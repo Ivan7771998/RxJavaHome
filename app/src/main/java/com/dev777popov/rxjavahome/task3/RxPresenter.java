9517f697a9da0cd74c9544c4f222ec735efca4bb
Popov Ivan <>
2020-07-04 13:43:56 +0300

    finish homework3

--- app/src/main/java/com/dev777popov/rxjavahome/task3/RxPresenter.java
+++ app/src/main/java/com/dev777popov/rxjavahome/task3/RxPresenter.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Single;
+import io.reactivex.SingleOnSubscribe;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxPresenter {
@@ -15,20 +17,25 @@ public class RxPresenter {
 
     public Observable<String> getPackageData() {
 
-        return Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(ObservableEmitter<String> emitter) {
-                try {
-                    for (int i = 0; i < 10; i++) {
-                        TimeUnit.SECONDS.sleep(1);
-                        String data = "data: " + i;
-                        Log.d(TAG, "getPackageData: " + Thread.currentThread().getName() + ": " + data);
-                        emitter.onNext(data);
-                    }
-                } catch (InterruptedException e) {
-                    Log.d(TAG, "getPackageData: unsubscribed");
+        return Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String data = "data: " + i;
+                    Log.d(TAG, "getPackageData: " + Thread.currentThread().getName() + ": " + data);
+                    emitter.onNext(data);
                 }
+            } catch (InterruptedException e) {
+                Log.d(TAG, "getPackageData: unsubscribed");
             }
+        }).subscribeOn(Schedulers.io());
+    }
+
+    public Single<String> getPackageDataSingle() {
+        return Single.create((SingleOnSubscribe<String>) emitter -> {
+            String data = "data: " + 1;
+            Log.d(TAG, "getPackageDataSingle: " + Thread.currentThread().getName() + ": " + data);
+            emitter.onSuccess(data);
         }).subscribeOn(Schedulers.io());
     }
 }
